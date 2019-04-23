bc0549d7dade643f3e7f888c806fd20b687c1faa
Brian Tobal <brian.tobal@gmail.com>
2019-04-23 00:23:46 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+
 require 'pry'
 
 class Song
@@ -18,13 +19,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
